2cf5a3ebbb37e7e2ad5a7a189b0cc4f1d4745647
cgimenez-prog <clara@gimenez.com.uy>
2026-08-22 13:07:53 -0300

    Agrego respuestas de multiple opción y renombro archivos

---
+++ Clase_2_diccionario_caja.py
@@ -0,0 +1,10 @@
+# Diccionario catacteristicas objetos caja
+datos_caja = {
+    "nro_objeto": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    "tamaño": ["Mediano", "Chico", "Mediano", "Chico", "Grande", "Mediano", "Mediano", "Chico", "Chico", "Grande"],
+    "textura": ["Peludo", "Liso", "Liso", "Liso", "Liso con agujeros", "Liso con un gran agujero", "Liso", "Liso", "Particular", "Liso"],
+    "forma": ["Ovalado", "Cuadrado", "Rectangulo largo", "Particular", "Rectangular", "Circular", "Rectangulo", "Hexagonal", "Particular", "Rectangulo"],
+    "tiene_movimientos": ["No", "Si", "No", "No", "No", "No", "No", "Si", "Si", "Si"],
+    "tiene_agujeros": ["No", "No", "No", "No", "Si", "Si", "Si", "Si", "No", "No"],
+    "peso_gramos": [100, 400, 200, 50, 350, 200, 300, 200, 300, 400]
+}
